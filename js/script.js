560c3f42268dc7cb017e8273e095756fa0f86823
romancazac <70430456+romancazac@users.noreply.github.com>
2020-12-13 17:14:35 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+    $('.aside-menu__burger').click(function(event) {
+        $('.items-menu,.aside-menu__burger').toggleClass('active');
+        
+       
+    });
+  
+    
+    
+   
+});
+
+let menuParents = document.querySelectorAll('.menu-page__parent');
+
+for (let index = 0; index < menuParents.length; index++) {
+    const menuParent = menuParents[index];
+    menuParent.addEventListener("mouseenter", function (e){
+        menuParent.classList.add('active');
+    });
+    menuParent.addEventListener("mouseleave", function (e){
+        menuParent.classList.remove('active');
+    });
+    
+}
+
+
+let searchBtn = document.querySelector('.search__btn');
+let menuSearch = document.querySelector('.menu-search');
+
+searchBtn.addEventListener("click", function (e) {
+    searchBtn.classList.toggle('open');
+    menuSearch.classList.toggle('open');
+    
+    
+ });
+
+
+ var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 5,
+    freeMode:true,
+    allowTouchMove:true,
+    noSwiping:true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: false,
+  });
+  var galleryTop = new Swiper('.gallery-top', {
+    autoHeight: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    }
+  });
+
+
+  var swiper = new Swiper('.products', {
+    pagination: {
+      el: '.products-body__pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.products-body__next',
+      prevEl: '.products-body__prev',
+    },
+  });
+  
+  var swiper = new Swiper('.brends__body', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.brends__next',
+      prevEl: '.brends__prev',
+    },
+  });
+  const headeBurger = document.querySelector('.header-top__burger');
+  const headerMenu = document.querySelector('.header-top__nav');
+
+  
+    headeBurger.addEventListener("click", function (e) {
+      headeBurger.classList.toggle('active');
+      headerMenu.classList.toggle('active');
+    });
+ 
+  
